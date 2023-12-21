c6c43427d1240d53d8bcc692cd22fc8c00180022
Tim Kieu <timkieu8288@gmail.com>
2023-12-21 17:34:49 +0700

    Update route.ts

--- app/api/hi/route.ts
+++ app/api/hi/route.ts
@@ -1,18 +1,17 @@
-import type { NextRequest, NextResponse } from 'next';
- 
+
 type ResponseData = {
   message: string
 }
  
 export const runtime = "edge";
 export async function GET(
-  req: NextRequest,
-  res: NextResponse<ResponseData>
+  req:   Request
 ) {
-  // res.status(200).json({ message: 'Hello  from API route Next.js!' })
+
   return new Response('Hello, Next.js edge runtime!', {
     status: 200,
     headers: { 'Set-Cookie': `token=afsdfsdflkj` },
   })
 }
+
 
